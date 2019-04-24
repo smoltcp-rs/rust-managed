313f7911fe778be9cb1fc2a1f1785bf799600764
Marc-Andre Lureau <marcandre.lureau@gmail.com>
2019-04-24 15:00:53 +0200

    Do not constrain Owned variants to 'static lifetime.
    This unnecessarily restricts the contents of the container to have
    a 'static lifetime, even if otherwise there is no such requirement.

--- src/slice.rs
+++ src/slice.rs
@@ -71,7 +71,7 @@ from_unboxed_slice!(0,  1,   2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 
                     16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31);
 
 #[cfg(any(feature = "std", feature = "alloc"))]
-impl<T: 'static> From<Vec<T>> for ManagedSlice<'static, T> {
+impl<'a, T: 'a> From<Vec<T>> for ManagedSlice<'a, T> {
     fn from(value: Vec<T>) -> Self {
         ManagedSlice::Owned(value)
     }
